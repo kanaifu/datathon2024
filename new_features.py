29551106b38c5d2034d636faf4f5ad7d98af1022
Sanya Garg <sanya.garg@gmail.com>
2024-01-20 01:02:44 -0600

    hi2

--- new_features.py
+++ new_features.py
@@ -14,8 +14,14 @@ def euclid_surface_bh_dist(df):
     df['surface_bottom_dist'] = ((df['surface_x'] - df['bh_x'])**2 + (df['surface_y'] - df['bh_y'])**2)**0.5
     return df
 
-def euclid_surface_bh_dist(df):
+def euclid_toe_dist(df):
     df['toe_dist'] = ((df['horizontal_midpoint_x'] - df['horizontal_toe_x'])**2 + (df['horizontal_midpoint_y'] - df['horizontal_toe_y'])**2)**0.5
     return df
 
+def surface_bottom_angle(df):
+   df['surface_bottom_angle'] = np.arctan2(df['surface_y'] - df['bh_y'], df['surface_x'] - df['bh_x'])
+   return df
 
+def toe_angle(df):
+   df['toe_angle'] = np.arctan2(df['horizontal_midpoint_y'] - df['horizontal_toe_y'], df['horizontal_midpoint_x'] - df['horizontal_toe_x'])
+   return df
